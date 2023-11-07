f3126bb22c22577b19911f259ed5dbf06723426d
ben <ben.mcevoy@gmail.com>
2023-11-07 11:03:37 +1100

    autofire on X

--- src/x360c64/main.c
+++ src/x360c64/main.c
@@ -8,11 +8,11 @@
 #include "pico/stdlib.h"
 #include "tusb.h"
 
-extern void hid_app_init(JoyPort_t*);
+extern void hid_app_init(JoyPort_t *);
 extern void hid_app_task(void);
 
 // this is global context
-static JoyPort_t* _context;
+static JoyPort_t *_context;
 
 static int hz = 1000;
 static repeating_timer_t _timer;
@@ -23,13 +23,15 @@ static const uint GPIO_LEFT = 3;
 static const uint GPIO_RIGHT = 27;
 static const uint GPIO_FIRE = 28;
 
-static void debug_context() {
+static void debug_context()
+{
     // slow this down to a reasonable level for stdio
     static uint8_t delay = 100;
 
     delay--;
 
-    if (delay == 0) {
+    if (delay == 0)
+    {
         delay = 100;
         printf("DPAD:%03u A:%03u B:%03u X:%03u Y:%03u POT1_X:%03u POT1_Y:%03u POT2_X:%03u POT2_Y:%03u", _context->dpad, _context->A,
                _context->B, _context->X, _context->Y, _context->POT1_X, _context->POT1_Y, _context->POT2_X, _context->POT2_Y);
@@ -39,64 +41,80 @@ static void debug_context() {
     }
 }
 
-bool sampler_callback(repeating_timer_t* rt) {
-
+bool sampler_callback(repeating_timer_t *rt)
+{
     const uint8_t THRESHOLD = 48;
+    const uint8_t AUTOFIRE_DELAY = 200;
+    const uint8_t AUTOFIRE_TRIGGER = 20;
+    static uint8_t _autoFireDelay = AUTOFIRE_DELAY;
 
     debug_context();
-    
-    // TODO: test this.  add in POT2
+
+    // TODO: add in POT2?
     bool isUp = (_context->dpad == 7) || (_context->dpad == 0) || (_context->dpad == 1) || _context->B || (_context->POT1_Y < THRESHOLD);
-    bool isDown = (_context->dpad == 3) || (_context->dpad == 4)|| (_context->dpad == 5) || (_context->POT1_Y > (255 - THRESHOLD));
-    bool isRight = (_context->dpad == 1) || (_context->dpad == 2)|| (_context->dpad == 3) || (_context->POT1_X > (255 - THRESHOLD));
-    bool isLeft = (_context->dpad == 5) || (_context->dpad == 6)|| (_context->dpad == 7) || (_context->POT1_X < THRESHOLD);
-    
+    bool isDown = (_context->dpad == 3) || (_context->dpad == 4) || (_context->dpad == 5) || (_context->POT1_Y > (255 - THRESHOLD));
+    bool isRight = (_context->dpad == 1) || (_context->dpad == 2) || (_context->dpad == 3) || (_context->POT1_X > (255 - THRESHOLD));
+    bool isLeft = (_context->dpad == 5) || (_context->dpad == 6) || (_context->dpad == 7) || (_context->POT1_X < THRESHOLD);
+
     gpio_put(GPIO_UP, isUp);
     gpio_put(GPIO_DOWN, isDown);
     gpio_put(GPIO_LEFT, isLeft);
     gpio_put(GPIO_RIGHT, isRight);
-    
-    gpio_put(GPIO_FIRE, _context->A);
+
+    _autoFireDelay--;
+
+    bool isAutoFire = _context->X && _autoFireDelay < AUTOFIRE_TRIGGER;
+
+    if (_autoFireDelay == 0)
+        _autoFireDelay = AUTOFIRE_DELAY;
+
+    gpio_put(GPIO_FIRE, _context->A || isAutoFire);
 
     return true;
 }
 
-void blink_led(void) {
+void blink_led(void)
+{
     const uint32_t interval_ms = 1000;
     static uint32_t start_ms = 0;
 
     static bool led_state = false;
 
     // Blink every interval ms
-    if (board_millis() - start_ms < interval_ms) return;
+    if (board_millis() - start_ms < interval_ms)
+        return;
     start_ms += interval_ms;
 
     board_led_write(led_state);
     led_state = 1 - led_state;
 }
 
-void context_init() {
+void context_init()
+{
     _context = malloc(sizeof(JoyPort_t));
 
     _context->A = 0;
     _context->B = 0;
     _context->X = 0;
-    _context->dpad = 0;
+    _context->dpad = 8; // 8 is released
     _context->POT1_X = 128;
     _context->POT1_Y = 128;
     _context->POT2_X = 128;
     _context->POT2_Y = 128;
 }
 
-void sampler_init() {
+void sampler_init()
+{
     // negative timeout means exact delay (rather than delay between callbacks)
     if (!add_repeating_timer_us(-1000000 / hz, sampler_callback, NULL,
-                                &_timer)) {
+                                &_timer))
+    {
         printf("Failed to add timer\n");
     }
 }
 
-void gpio_joyport_init() {
+void gpio_joyport_init()
+{
     gpio_init(GPIO_UP);
     gpio_set_dir(GPIO_UP, GPIO_OUT);
 
@@ -113,7 +131,8 @@ void gpio_joyport_init() {
     gpio_set_dir(GPIO_FIRE, GPIO_OUT);
 }
 
-int main(void) {
+int main(void)
+{
     stdio_init_all();
     sleep_ms(100);
 
@@ -129,7 +148,8 @@ int main(void) {
 
     sampler_init();
 
-    while (1) {
+    while (1)
+    {
         // TODO: can something go on the other core? gpio timer?
         tuh_task();
         blink_led();
@@ -139,10 +159,12 @@ int main(void) {
     return 0;
 }
 
-void tuh_mount_cb(uint8_t dev_addr) {
+void tuh_mount_cb(uint8_t dev_addr)
+{
     printf("A device with address %d is mounted\r\n", dev_addr);
 }
 
-void tuh_umount_cb(uint8_t dev_addr) {
+void tuh_umount_cb(uint8_t dev_addr)
+{
     printf("A device with address %d is unmounted \r\n", dev_addr);
 }
